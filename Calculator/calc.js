54c5496c98f9d5f43a6da07e84d30ab0221da15a
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2024-10-20 13:17:55 +0600

    Update calc.js

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -8,11 +8,29 @@ function clearDisplay() {
     display11.value = "";
 }
 
+function backspace() {
+    display11.value = display11.value.slice(0, -1);
+}
+
 function calculate() {
     try {
         display11.value = eval(display11.value);
-    }
-    catch (error) {
+    } catch (error) {
         display11.value = "Error";
     }
 }
+
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+
+    if ((key >= '0' && key <= '9') || key === '.' || key === '+' || key === '-' || key === '*' || key === '/') {
+        appendToDisplay(key);
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        backspace();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
+});
